45b3fb29b7dbc1e067c375f71766f3be0dc490b3
papuadi <rahmatullahpapuadi@gmail.com>
2023-07-03 16:50:18 +0700

    update 2

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
   }
 }
