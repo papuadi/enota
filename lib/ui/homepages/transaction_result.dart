dc9f85cd3053a14b1ba2818ec9131cf8314e0b9f
papuadi <rahmatullahpapuadi@gmail.com>
2023-07-03 17:18:25 +0700

    updae 2

--- lib/ui/homepages/transaction_result.dart
+++ lib/ui/homepages/transaction_result.dart
@@ -37,166 +37,169 @@ class TransactionPageResult extends StatelessWidget {
             },
           ),
         ),
-        body: Container(
-          color: Color(0xff00B8E2), // Warna latar belakang body
-          child: Column(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 23, horizontal: 20),
-                child: Table(
-                  defaultColumnWidth: FixedColumnWidth(100.0),
-                  border: TableBorder.all(
-                      color: Colors.black, width: 1.0), // Tambahkan border
-                  children: [
-                    TableRow(
-                      children: [
-                        TableCell(
-                          child: Container(
-                            height: 50.0,
-                            color: Colors.white,
-                            child: Center(
-                              child: Text(
-                                'Jumlah',
-                                style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Container(
+            color: Color(0xff00B8E2), // Warna latar belakang body
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 23, horizontal: 20),
+                  child: Table(
+                    defaultColumnWidth: FixedColumnWidth(100.0),
+                    border: TableBorder.all(
+                        color: Colors.black, width: 1.0), // Tambahkan border
+                    children: [
+                      TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              height: 50.0,
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  'Jumlah',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        TableCell(
-                          child: Container(
-                            height: 50.0,
-                            color: Colors.white,
-                            child: Center(
-                              child: Text(
-                                'Item',
-                                style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
+                          TableCell(
+                            child: Container(
+                              height: 50.0,
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  'Item',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        TableCell(
-                          child: Container(
-                            height: 50.0,
-                            color: Colors.white,
-                            child: Center(
-                              child: Text(
-                                'Harga',
-                                style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
+                          TableCell(
+                            child: Container(
+                              height: 50.0,
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  'Harga',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        TableCell(
-                          child: Container(
-                            height: 50.0,
-                            color: Colors.white,
-                            child: Center(
-                              child: Text(
-                                'Total',
-                                style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
+                          TableCell(
+                            child: Container(
+                              height: 50.0,
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  'Total',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        TableCell(
-                          child: Container(
-                            height: 50.0,
-                            color: Colors.white,
-                            child: Center(
-                              child: Text(
-                                '2',
-                                style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          TableCell(
+                            child: Container(
+                              height: 50.0,
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  '2',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        TableCell(
-                          child: Container(
-                            height: 50.0,
-                            color: Colors.white,
-                            child: Center(
-                              child: Text(
-                                'Gula Pasir',
-                                style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
+                          TableCell(
+                            child: Container(
+                              height: 50.0,
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  'Gula Pasir',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        TableCell(
-                          child: Container(
-                            height: 50.0,
-                            color: Colors.white,
-                            child: Center(
-                              child: Text(
-                                '10000',
-                                style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
+                          TableCell(
+                            child: Container(
+                              height: 50.0,
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  '10000',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        TableCell(
-                          child: Container(
-                            height: 50.0,
-                            color: Colors.white,
-                            child: Center(
-                              child: Text(
-                                '20000',
-                                style: GoogleFonts.poppins(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
+                          TableCell(
+                            child: Container(
+                              height: 50.0,
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  '20000',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Jumlah = 20000',
-                      textAlign: TextAlign.end,
-                      style: GoogleFonts.poppins(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Jumlah = 20000',
+                        textAlign: TextAlign.end,
+                        style: GoogleFonts.poppins(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
